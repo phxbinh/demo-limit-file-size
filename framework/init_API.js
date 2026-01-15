d75301a5d2ae4b9382a6efb95e154602e4a997ec
phxbinh <phxbinh@gmail.com>
2026-01-15 22:27:48 +0700

    Add files via upload

---
+++ framework/init_API.js
@@ -0,0 +1,22 @@
+
+const {
+  h,
+  render,
+  Fragment,
+  memo } = App.VDOM;
+
+const {
+  useState,
+  useEffect,
+  useReducer,
+  useRef,
+  useCallback,
+  useMemo } = App.Hooks;
+
+const {
+  Link,
+  navigateTo,
+  addRoute,
+  init,
+  afterEach } = App.Router;
+
